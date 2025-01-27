4e15187435a68e694438fef9969597d947b74777
emperorsixpacks <andrewdvd10@gmail.com>
2025-01-27 21:44:05 +0100

    feat: added read and write to file

--- dailbot/main.py
+++ dailbot/main.py
@@ -7,8 +7,10 @@ from starlette import middleware
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 from twilio.rest import Client
+from twilio.rest.conversations.v1.configuration import webhook
 
 from dailbot.settings import AirtableSettings, AppSettings, TailwindSettings
+from dailbot.utils import create_new_webhook, read_secret, write_secret
 
 load_dotenv(".env")
 app_settings = AppSettings()
@@ -31,25 +33,15 @@ middlewares = [
         #        allow_headers=["*"],
     ),
 ]
-"""
-airtable_api.add_webhook(
-    notify_url=WEBHOOK_URL,
-    spec={
-        "options": {
-            "filters": {
-                "dataTypes": ["tableData"],
-                "recordChangeScope": "tblZbNhBmDblSUXR1",
-            }
-        }
-    },
-)
-"""
+
+# res, new_webhook = create_new_webhook(airtable_api, WEBHOOK_URL)
+
 app = FastAPI(middleware=middlewares)
 
 
 @app.post("/webhook", response_model=None)
 async def airtable_notification(request: Request):
-    print(request)
+    await request.body()
     return 200
 
 
--- dailbot/utils.py
+++ dailbot/utils.py
@@ -1,31 +1,57 @@
 from __future__ import annotations
 
+import hashlib
+import hmac
 import os
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 if TYPE_CHECKING:
     from pyairtable.api import Base
+    from pyairtable.models.webhook import CreateWebhookResponse
 
 
 def return_app_dir():
     return os.path.dirname(os.path.abspath(__file__))  # os.path.abspath(path=__file__))
 
 
-def create_new_webhook(airtable_client: Base, webhook_url):
+BASE_DIR = os.path.dirname(os.path.abspath(return_app_dir()))
+
+
+def create_new_webhook(
+    airtable_client: Base, webhook_url
+) -> Optional[CreateWebhookResponse]:
     for webhook in airtable_client.webhooks():
         if webhook.notification_url == webhook_url:
-            return
-    airtable_client.add_webhook(
+            return None
+    res = airtable_client.add_webhook(
         notify_url=webhook_url,
         spec={
             "options": {
                 "filters": {
                     "dataTypes": ["tableData"],
                     "recordChangeScope": "tblZbNhBmDblSUXR1",
+                    "changeTypes": ["create", "update", "delete"],
                 }
             }
         },
     )
+    return res
 
 
-BASE_DIR = os.path.dirname(os.path.abspath(return_app_dir()))
+def write_secret(secret: str):
+    with open(os.path.join(BASE_DIR, "secret.txt"), "w") as file:
+        file.write(secret)
+        file.close()
+
+def read_secret() -> str:
+    with open(os.path.join(BASE_DIR, "secret.txt"), "r") as file:
+        data = file.read()
+        file.close()
+        return data
+
+
+def verify_signature(payload, received_signature):
+    computed_signature = hmac.new(
+        whatsapp_config.whatsapp_token.encode(), payload, hashlib.sha256
+    ).hexdigest()
+    return hmac.compare_digest(computed_signature, received_signature)
